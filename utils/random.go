d2d9477f84a064f3aae850ba26b6cbed9f12d86d
Hamza <hamza.siddiqui4747@gmail.com>
2026-02-22 15:20:26 +0900

    unit tests completed for accounts

--- utils/random.go
+++ utils/random.go
@@ -1,1 +1,44 @@
 package utils
+
+import (
+	"math/rand"
+	"strings"
+)
+
+const alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Intn(1000)
+}
+
+// Generate random integer b/w min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// Generate random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabets)
+
+	for i := 0; i < n; i++ {
+		c := alphabets[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "JPY"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}

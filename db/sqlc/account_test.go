fd8b019dcb72c1f7d96c14581cbfee22ee13c675
Hamza <hamza.siddiqui4747@gmail.com>
2026-02-22 13:01:34 +0900

    test create account success

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,1 +1,27 @@
 package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "Hamza",
+		Currency: "JPY",
+		Balance:  199,
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(t, arg.Balance, account.Balance)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}

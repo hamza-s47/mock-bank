8f8dbde59bc1d23ed2e86873df427cc298f03e35
Hamza <hamza.siddiqui4747@gmail.com>
2026-03-28 08:57:29 +0900

    env set

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/hamza-s47/mock-bank/api"
 	db "github.com/hamza-s47/mock-bank/db/sqlc"
+	"github.com/hamza-s47/mock-bank/utils"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the database", err)
 	}
@@ -24,8 +24,9 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
+
 }
